13a3fcade4c8d5445ac90723468a8a2bd8115e11
Kobi Gurkan <kobigurk@gmail.com>
2019-10-26 23:34:48 +0300

    commits dist

--- dist/test/bls.js
+++ dist/test/bls.js
@@ -41,9 +41,10 @@ describe('bls', function () {
     });
     it('should test proof of possession', function () {
         var privateKey = new Buffer('60515f8c59451e04ab4b22b3fc9a196b2ad354e61aeca61256ab3d7b3468100b', 'hex');
+        var address = new Buffer('60515f8c59451e04ab4b22b3fc9a196b2ad354e6', 'hex');
         var publicKeyHex = __1.BLS.privateToPublicBytes(privateKey).toString('hex');
         chai_1.expect(publicKeyHex).to.equal('c91e3ae9b4380143652cf199faeeab471e639c969e55275cf3bae66aad5d1c6d6f8bab3cb43fd20a78297cb0a8afe880');
-        var popHex = __1.BLS.signPoP(privateKey).toString('hex');
+        var popHex = __1.BLS.signPoP(privateKey, address).toString('hex');
         chai_1.expect(popHex).to.equal('e9d004d288c88ed669f6156951d736c5e51d79ebb8627ebd16fc24ab625270a44ebd1c9bbb90df1530a68f0e945967006b6b374b30f17389f3e2dedf9a2db8c33abfbc3331d3654702f2e27536cb914088db2f31696c10bd2d53d35b8fb7e780');
     });
     it('should test many proofs of possession', function () {
@@ -55,7 +56,8 @@ describe('bls', function () {
                 var privateKey = new Buffer(line[0], 'hex');
                 var publicKeyHex = __1.BLS.privateToPublicBytes(privateKey).toString('hex');
                 chai_1.expect(publicKeyHex).to.equal(line[1]);
-                var popHex = __1.BLS.signPoP(privateKey).toString('hex');
+                var address = new Buffer('60515f8c59451e04ab4b22b3fc9a196b2ad354e6', 'hex');
+                var popHex = __1.BLS.signPoP(privateKey, address).toString('hex');
                 chai_1.expect(popHex).to.equal(line[2]);
             }
             catch (e) {
